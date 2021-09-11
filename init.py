d5ec63cf459fe9002b8b0a80e30b6778b60e29ea
한중명 <gkswndaud@gmail.com>
2021-09-11 18:32:38 +0900

    study python basic

---
+++ init.py
@@ -0,0 +1,60 @@
+# 문자열 출
+
+hello = '안녕 파이썬'
+
+print(hello)
+print('\n')
+
+#문자의 연산
+head='python'
+tail=' is fun!'
+
+print((head + tail) *5)
+print('\n')
+
+# 정수의 연산
+
+a = 2;
+b = 3;
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a % b)
+print(a ** b)
+print(a // b)
+print('\n')
+
+
+#논리, 비교연산자
+print(a < 10 and b < 50)
+print(a < 10 or b > 50)
+print(not b < 50)
+print('\n')
+
+
+#예약어의 종류
+
+import keyword
+print(keyword.kwlist)
+
+print('\n')
+
+
+
+#다양한 타입의 변수
+
+num = 100
+print(type(num))
+
+name = '홍길동'
+print(type(name))
+
+num = 2.2
+print(type(num))
+
+result=True
+print(type(result))
+
+
